0b2798198358476057cb03e739058fb6907cc4b9
unknown <mmankar.personnel@gmail.com>
2023-03-12 22:48:49 +0530

    Test new code

---
+++ src/com/gitdemo/test1/Test1.java
@@ -0,0 +1,17 @@
+package com.gitdemo.test1;
+
+
+/*echo "# TestingNewGitProject" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mmankar/TestingNewGitProject.git
+git push -u origin main
+*/
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("I am working!!!");
+	}
+}
